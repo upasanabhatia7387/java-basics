f3912291f8b68792f6d48230021f379a1e62f2f3
upasanabhatia7387 <125219018+upasanabhatia7387@users.noreply.github.com>
2024-05-13 06:10:58 -0700

    Date Time API

---
+++ DataTime.java
@@ -0,0 +1,71 @@
+package DataTimeExample;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DataTime {
+	public static void LocalDateTimeApi() {
+		
+		//the current date
+		LocalDate date = LocalDate.now();
+		System.out.println("the current date is: "+date);
+		
+		//the current time
+		LocalTime time = LocalTime.now();
+		System.out.println("the current time is: "+time);
+		
+		//will give us the current time and date
+		LocalDateTime current = LocalDateTime.now();
+		System.out.println("the current date time is: "+current);
+		
+		//to print a particular format
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		
+		String formatedDateTime = current.format(format);
+		
+		System.out.println("in formatted manner "+formatedDateTime);
+		
+		//to get current zone
+		ZonedDateTime currentZone = ZonedDateTime.now();
+		System.out.println("the current zone is: "+currentZone.getZone());
+		
+		//getting time zone of specific place
+		//we use withZoneSameInstant() is
+		//used to return a copy of this date-time
+		//with a different time-zone
+		//retaining the instant
+		
+		ZoneId tokyo = ZoneId.of("Asia/Tokyo");
+		
+		ZonedDateTime tokyoZone = currentZone.withZoneSameInstant(tokyo);
+		System.out.println("the tokyo zone is: "+tokyoZone);
+		
+		
+		//printing months days and seconds
+		Month month = current.getMonth();
+		int day = current.getDayOfMonth();
+		int seconds = current.getSecond();
+		System.out.println("Month :" + month + "day: "+ day+ "seconds: "+ seconds);
+		
+		//printing some specified date
+		LocalDate date2 = LocalDate.of(1950, 1, 26);
+		System.out.println("the republic day:"+date2);
+		
+		//printing the date with current time
+		LocalDateTime specificDate = current.withDayOfMonth(24).withYear(2016);
+		System.out.println("specific date with:"+"current time"+specificDate);
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			LocalDateTimeApi();
+		
+	}
+	
+
+}
